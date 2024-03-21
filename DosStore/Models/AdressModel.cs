6c32f55a440a67fde865fff2948518e7ab5350b2
ROG <luan.roger.2003@gmail.com>
2024-03-20 21:34:31 -0300

    feat: create models

---
+++ DosStore/Models/AdressModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DosStore.Models;
+
+[Table("enderecos")]
+public class AdressModel
+{
+    [Key]
+    public int id { get; set; }
+    
+    [Required]
+    public string rua { get; set; }
+    
+    [Required]
+    public string numero { get; set; }
+    
+    [Required]
+    public string complemento { get; set; }
+    
+    [Required]
+    public string bairro { get; set; }
+    
+    [Required]
+    public string cidade { get; set; }
+    
+    [Required]
+    public string cep { get; set; }
+    
+    [Required]
+    public string pontoReferencia { get; set; }
+    
+    [Required]
+    public ClienteModel cliente { get; set; }
+}
---
+++ DosStore/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DosStore.Models;
+
+[Table("user")]
+public class UserModel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int id { get; set; }
+    
+    [Required]
+    public string login { get; set; }
+    
+    [Required]
+    public string senha { get; set; }
+}
