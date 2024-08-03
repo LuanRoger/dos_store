bce4981248b1f507002397d380d292a261919a37
ROG <luan.roger.2003@gmail.com>
2024-08-03 11:40:11 -0300

    feat: create view to show products and pick one

---
+++ src/Models/Read/MinimalReadProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DosStore.Models.Read;
+
+public class MinimalReadProductModel
+{
+    public required int id { get; init; }
+    public required string nome { get; init; }
+    public required double valor { get; init; }
+    public required int quantidade { get; init; }
+}
---
+++ src/Views/PickProductMenuView.cs
@@ -0,0 +1,30 @@
+﻿using DosStore.Controller;
+using DosStore.Models.Read;
+using DosStore.Utils;
+using Spectre.Console;
+
+namespace DosStore.Views;
+
+public class PickProductMenuView : IResultedFlowMenu<int>
+{
+    private readonly ProductController _productController = new();
+    
+    public async Task<int> StartFlow()
+    {
+        var products = await _productController.GetAllProducts();
+        bool areProduct = products.Any();
+        if(!areProduct)
+            return -1;
+        
+        MinimalReadProductModel selectedProduct = AnsiConsole.Prompt(
+            new SelectionPrompt<MinimalReadProductModel>()
+                .Title("Qual produto você deseja escolher?")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Mova para cima e para baixo para ver mais opções)[/]")
+                .AddChoices(products)
+                .UseConverter(MinimalReadProductModelFormater.FormatProduct)
+        );
+
+        return selectedProduct.id;
+    }
+}
