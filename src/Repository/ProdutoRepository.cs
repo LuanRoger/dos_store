722626a877beac8e6b3a669c6e6accbd840a88b1
ROG <luan.roger.2003@gmail.com>
2024-08-03 11:39:13 -0300

    feat: create `ProductController`

--- src/Repository/ProdutoRepository.cs
+++ src/Repository/ProdutoRepository.cs
@@ -1,13 +1,47 @@
 ﻿using DosStore.Context;
 using DosStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DosStore.Repository;
 
 public class ProdutoRepository
 {
+    public async Task<IReadOnlyList<ProdutoModel>> GetAllProducts()
+    {
+        await using AppDbContext db = new();
+        return await db.produtos.ToListAsync();
+    }
+    
+    public async Task<ProdutoModel?> GetProdutoById(int id)
+    {
+        await using AppDbContext db = new();
+        return await db.produtos.FindAsync(id);
+    }
+    
     public async Task RegisterProduct(ProdutoModel newProduct)
     {
         await using AppDbContext db = new();
         await db.produtos.AddAsync(newProduct);
+        await db.SaveChangesAsync();
+    }
+    
+    public async Task DeleteProduct(ProdutoModel product)
+    {
+        await using AppDbContext db = new();
+        db.produtos.Remove(product);
+        await db.SaveChangesAsync();
+    }
+    
+    public async Task<int?> UpdateProductAmount(int id, int ammount)
+    {
+        await using AppDbContext db = new();
+        ProdutoModel? product = await db.produtos.FindAsync(id);
+        if(product is null)
+            return null;
+        
+        product.quantidade = ammount;
+        await db.SaveChangesAsync();
+        
+        return product.quantidade;
     }
 }
